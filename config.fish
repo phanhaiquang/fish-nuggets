b5b3c314b64b1a683b8be1c22413a6f712559e66
Phan Hai Quang <phanhaiquang@gmail.com>
2013-09-11 15:41:08 +0700

    init version

---
+++ config.fish
@@ -0,0 +1,7 @@
+. ~/.config/fish/alias.fish
+. ~/.config/fish/git_alias.fish
+
+. ~/.config/fish/project_env.fish
+
+rvm > /dev/null
+
---
+++ project_env.fish
@@ -0,0 +1,1 @@
+set -x buzzstarter "/home/quangphan/project/rails/buzzstarter"
